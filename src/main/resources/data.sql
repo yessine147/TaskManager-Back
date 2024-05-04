27228db5f86fab71e6e02468b402ab1d93e928a8
yessine-ben-jemaa <yessine.benjemaa47@gmail.com>
2024-05-04 15:14:43 +0100

    add data sql file and insert some records to database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO TASKS_LIST (title, description, completed) values ('task1','task1 description','true');
+INSERT INTO TASKS_LIST (title, description, completed) values ('task2','task2 description','true');
+INSERT INTO TASKS_LIST (title, description, completed) values ('task3','task3 description','true');
+INSERT INTO TASKS_LIST (title, description, completed) values ('task4','task4 description','false');
+INSERT INTO TASKS_LIST (title, description, completed) values ('task5','task5 description','true');
